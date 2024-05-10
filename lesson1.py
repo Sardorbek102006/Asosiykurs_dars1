0bfbb4f64232e8f50cebd6186f486a4b654b73c7
Sardorbek <ergashevsardorbek031@gmail.com>
2024-05-10 11:23:30 +0300

    homework

---
+++ lesson1.py
@@ -0,0 +1,36 @@
+# def number_split(num):
+#     lst = []
+#     m = num // 2
+#     lst.append(m)
+#     lst.append(m)
+#     return lst
+#
+# print(number_split(4))
+# print(number_split(10))
+# print(number_split(11))
+# print(number_split(-9))
+
+#inp = input("yozing; ")
+
+# def asc_desc_none(lst,s):
+#     if s == "Asc":
+#         lst.sort()
+#         return lst
+#     elif s == "des":
+#         lst.sort(reverse=True)
+#         return lst
+#     return lst
+#
+# print(asc_desc_none([4,3,2,1], "Asc"))
+# print(asc_desc_none([7,8,11,66], "des"))
+# print(asc_desc_none([1,2,3,4], "none"))
+
+def seriec_recictence(lst):
+    m = sum(lst)
+    if m > 1:
+        return m, "ohms"
+    return m, "ohm"
+
+print(seriec_recictence([1,5,6,3]))
+print(seriec_recictence([16,3.5,6]))
+print(seriec_recictence([0.5,0.5]))
